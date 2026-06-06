6c633ee4634851d6160405e3bc42f647ea1e2019
Sebona Haile <sebonahaile@Gmail.com>
2026-06-06 18:34:17 +0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
       <AppRoutes />
-    </BrowserRouter>
+    </>
   );
 }
 

fc18d8ab16b86c5d694aca84eaa0b48410be1d3b
Sebona Haile <sebonahaile@Gmail.com>
2026-06-06 17:30:35 +0300

    fix css types issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+
+      {/* 👇 PUT IT HERE (UNDER BrowserRouter) */}
+      <ScrollToTop />
+
+      <AppRoutes />
+
+    </BrowserRouter>
+  );
 }
 
 export default App;

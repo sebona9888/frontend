261836bf5e30a67f6efe99e11e4398f7258abef6
Sebona Haile <sebonahaile@Gmail.com>
2026-06-06 17:44:37 +0300

    test push

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
 
-     
+      {/* 👇 PUT IT HERE (UNDER BrowserRouter) */}
+      <ScrollToTop />
+
       <AppRoutes />
 
     </BrowserRouter>
